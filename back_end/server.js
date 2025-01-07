15c001382849a8b3a2fc7e5cad257bfd61684530
Divyansh kumar Chaturvedi <divyanshchaturvedi317@gmail.com>
2025-01-07 23:18:16 +0530

    first commit

---
+++ back_end/server.js
@@ -0,0 +1,34 @@
+import express from 'express'; // ye hai module js ka syntax .. ek common js bhi hota hai jisme require use krke krte hai
+
+const app = express();
+
+// app.get('/',(req,res) => {
+//     res.send('Server is ready');
+// });
+
+app.get('/api/jokes',(req,res)=> {
+    const jokes = [
+        {
+            id:1,
+            title:'A joke',
+            content:'This is joke'
+        },
+        {
+            id:2,
+            title:'Another joke',
+            content:'This is another joke'
+        },
+        {
+            id:3,
+            title:'A third joke',
+            content:'This is a third joke'
+        }
+    ];
+    res.send(jokes);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,() => {
+    console.log(`server at https://localhost:${port}`);
+});
